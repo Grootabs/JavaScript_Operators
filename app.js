76272225b5032f1f2985ea9d871b8f0178f846eb
Grootabs <abs122516@gmail.com>
2022-08-12 13:24:20 -0500

    Completed Exercise

--- app.js
+++ app.js
@@ -7,13 +7,60 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+let add = (a + b);
+let minus = (a - b);
+let multiply = (a * b);
+let dividing = (a / b);
+
+console.log (a);
+console.log (b);
+console.log (add);
+console.log (minus);
+console.log (multiply);
+console.log (dividing);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+// num + str = 1111
+// num + str2 = 11eleven
+// num + isPresent = 12
+// firstName + num = Frodo11
+// isPresent + str = true11
+// firstName + lastName = FrodoBaggins
+
+console.log (num + str);
+console.log (num + str2);
+console.log (num + isPresent);
+console.log (firstName + num);
+console.log (isPresent + str);
+console.log (firstName + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+isPresent = false;
+
+console.log (val == str3);
+console.log (val === str3);
+console.log (!isPresent);
+console.log ("eleven" == str4 && val >= str3);
+console.log (!isPresent || isPresent);
+console.log (0 == false);
+console.log (0 === false);
+console.log (0 != false);
+console.log (0 !== false);
